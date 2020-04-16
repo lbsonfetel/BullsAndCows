3bbbb89a8cf9b0cda5024adcf0829c8f10307702
lebinhson <lebinhson@outlook.com>
2020-04-16 17:57:39 +0700

    Create SecretNumber

--- BullsAndCows/Program.cs
+++ BullsAndCows/Program.cs
@@ -10,6 +10,44 @@ namespace BullsAndCows
     {
         static void Main(string[] args)
         {
+            int[] secretNumberArray = GenerateSecretNumber();
+            ShowSecretNumber(secretNumberArray);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowSecretNumber(int[] secretNumberArray)
+        {
+            foreach (int i in secretNumberArray)
+                Console.Write("{0 }", i);
+        }
+
+        private static int[] GenerateSecretNumber()
+        {
+            Random random = new Random();
+            int[] secretNumberArray = new int[4];
+
+            for (int i = 0; i < secretNumberArray.Length; i++)
+            {
+                bool isUnique = false;
+                int newNumber = 0;
+                while (!isUnique)
+                {
+                    newNumber = random.Next(10);
+                    isUnique = true;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (newNumber == secretNumberArray[j])
+                        {
+                            isUnique = false;
+                            break;
+                        }
+                    }
+                }
+                secretNumberArray[i] = newNumber;
+            }
+
+            return secretNumberArray;
         }
     }
 }
